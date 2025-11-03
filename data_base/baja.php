448f3f895ccae80b72549e31040f2df61c83daa0
Gabriel1406git <riverogabriel1406@gmail.com>
2025-11-03 10:24:24 -0300

    muestro los datos ingresados en una tabla en baja.php

--- data_base/baja.php
+++ data_base/baja.php
@@ -12,11 +12,69 @@
     <a href="baja.php"><button>BAJA PRODUCTOS</button></a>
     <a href="modificacion.php"><button>MODIFICACION PRODUCTOS</button></a><br>
 
+<table> 
+<tr><th>ID</th> 
+<th>Titulo</th> 
+<th>Marca</th> 
+<th>Categoria</th> 
+<th>Modelo</th> 
+<th>Caracteristica1</th> 
+<th>Caracteristica2</th> 
+<th>Caracteristica3</th> 
+<th>Precio</th> 
+</tr>
+
+<?php 
+
+include "conexion.php";
+
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "";
+$dataBase = "todo_motos";
+
+$conexion = conexion($servidor, $usuario, $contraseña, $dataBase);
+
+$out_datos = "select * from productos";
+
+$registros = mysqli_query($conexion,$out_datos);
+
+while($registro=mysqli_fetch_row($registros)){ 
+    ?><tr> 
+    
+        
+    <td><?php echo $registro[0]?></td> 
+    <td><?php echo $registro[1]?></td> 
+    <td><?php echo $registro[2]?></td> 
+    <td><?php echo $registro[3]?></td>
+    <td><?php echo $registro[4]?></td>
+    
+    <?php 
+    for ($i = 5; $i < 8; $i++) {
+    echo "<td>";
+    if ($registro[$i] == "") {
+        echo "---";
+    } else {
+        echo $registro[$i];
+    }
+    echo "</td>";
+    }
+    ?>
+
+    <td><?php echo $registro[8]?></td> 
+</tr> 
+<?php 
+} 
+?> 
+</table> 
+
+
+
 </body>
 </html>
 
 <?php 
-include "conexion.php";
+
 
 
 
@@ -59,30 +117,9 @@ else{
 
 */
 
-$servidor = "localhost";
-$usuario = "root";
-$contraseña = "";
-$dataBase = "todo_motos";
 
-$conexion = conexion($servidor, $usuario, $contraseña, $dataBase);
 
-$out_datos = "select * from productos";
 
-$registros = mysqli_query($conexion,$out_datos);
-
-while($registro = mysqli_fetch_row($registros))
-{
-    echo $registro[0]." ";
-    echo $registro[1]." ";
-    echo $registro[2]." ";
-    echo $registro[3]." ";
-    echo $registro[4]." ";
-    echo $registro[5]." ";
-    echo $registro[6]." ";
-    echo $registro[7]."<br>";
-    
-
-}
 
  
 ?>
