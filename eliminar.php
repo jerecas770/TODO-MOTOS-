e5a009c26d7fbc6e2a3e02b0cd8d6ad58fb31493
jere <jere.cas70@gmail.com>
2025-11-14 11:38:48 -0300

    Delete eliminar.php

--- eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php include('conexion.php');
-$id = $_GET['id'];
-if ($conn->query("DELETE FROM productos WHERE id_producto=$id")) {
-  echo "<p>✅ Producto eliminado correctamente.</p>";
-} else {
-  echo "<p>❌ Error: {$conn->error}</p>";
-}
-echo "<a href='administrarProductos.php'>Volver</a>";
-?>
